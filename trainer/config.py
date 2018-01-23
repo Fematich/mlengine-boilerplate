51316dbd40a5c31597a1d63b083f28de638b86b4
Juta Staes <juta.staes@ml6.eu>
2018-01-23 09:59:19 +0100

    updated config

--- trainer/config.py
+++ trainer/config.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
+# TODO change project_id and bucket
 PROJECT_ID = 'project-id'
 BUCKET = 'gs://bucket/'
-DATA_DIR = BUCKET + 'data/'
+
+DATA_DIR = 'gs://gdg-ml-at-scaledata/'
 TFRECORD_DIR = BUCKET + 'tfrecords/'
 MODEL_DIR = BUCKET + 'model/'
 MODEL_NAME = 'flowers'
