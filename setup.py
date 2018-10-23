5e3d55319c722995253e45d32a062192c0ca03ed
svetlana-v <21034626+svetlana-v@users.noreply.github.com>
2018-10-23 23:18:58 +0200

    Feature update (#9)
    * Fix depricated tf.train.framework.get_global_step(), comment printout
    
    * Update requirements
    
    * Update README, add json file for predictions
    
    * Update README

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
     version='0.1',
     author='Matthias Feys',
     author_email='matthiasfeys@gmail.com',
-    install_requires=['tensorflow==1.8.0',
-                      'tensorflow-transform==0.6.0'],
+    install_requires=['tensorflow==1.10.0',
+                      'tensorflow-transform==0.9.0'],
     packages=find_packages(exclude=['data', 'predictions']),
     description='ML Engine boilerplate code',
     url='https://github.com/Fematich/mlengine-boilerplate'
 )
+
